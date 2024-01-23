a4316f5c60f18db1d4d63654ec082aca629d588f
ramiz <ramiz90151@gmail.com>
2024-01-23 22:46:47 +0100

    exfordelete

--- src/GitDelete/GitDelete.java
+++ src/GitDelete/GitDelete.java
@@ -1,8 +1,10 @@
 package GitDelete;
 
+import zadacijavarash.uroven10zadaca2.SozdayemNeboskreb;
+
 public class GitDelete {
     public static void main(String[] args) {
         System.out.println("ok ok ");
-
+        System.out.println();
     }
 }
